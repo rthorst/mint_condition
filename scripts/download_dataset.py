a33a002a521735c968ab4d1bda69cae7c590dd13
Robert Thorstad <thorstadrs@gmail.com>
2020-01-16 09:03:08 -0500

    improvements to download_dataset.py

--- scripts/download_dataset.py
+++ scripts/download_dataset.py
@@ -5,7 +5,7 @@ import time
 from bs4 import BeautifulSoup
 import itertools
 import random 
-
+import shutil
 
 def download_beckett_htmls():
     """
@@ -13,10 +13,10 @@ def download_beckett_htmls():
     """
 
     base_url = "https://marketplace.beckett.com/search_new/?sport=185223&rowNum=250"
-    page_nums = range(1, 11)
+    page_nums = range(11, 51)
     conditions = ["NM", "MINT", "EX", "VG", "GOOD", "FAIR", "POOR"]
     
-    for page_num in range(1, 11):
+    for page_num in page_nums:
         for condition in conditions: 
 
             # Download page.
@@ -41,6 +41,7 @@ def download_beckett_htmls():
 def extract_individual_records():
     """ 
     Extract individual records, currently image URL and condition.
+    Also assign each record a unique numerical ID.
     Create data/individual_records.jsonl
     """
 
@@ -53,6 +54,7 @@ def extract_individual_records():
     data_p = os.path.join("..", "data", "htmls")
     html_fnames = os.listdir(data_p)
     random.shuffle(html_fnames)
+    id = 1 # counter: unique numerical ID for each record. 
     for html_fname in html_fnames:
 
         # Load HTML.
@@ -71,7 +73,10 @@ def extract_individual_records():
 
         # List all mpl2 tags which contain the image URLs.
         mpl2s = soup.findAll("li", {"class" : "mpL2"}) # contains image
-        assert len(mpl2s) == 250
+        try:
+            assert len(mpl2s) == 250
+        except:
+            print("NOT 250 records!!, have {}".format(len(mpl2s)))
 
         # Write output.
         for mpl2_tag in mpl2s:
@@ -84,10 +89,14 @@ def extract_individual_records():
 
                 # Write.
                 out = json.dumps({
+                    "id" : id, # unique numerical ID
                     "image_url" : img_url,
-                    "img_grade" : condition # same for every img in file
+                    "condition" : condition # same for every img in file
                 })
                 of.write(out + "\n")
+
+                # Increment numerical ID.
+                id += 1
 
             except Exception as e:
 
@@ -98,17 +107,93 @@ def extract_individual_records():
     of.close()
     print("Done, wrote {}".format(of_p))
 
-"""
-image.
-li
-data-original
+def clean_individual_records():
+    """
+    Clean individual records -- currently, skip images with no-image
+    placeholder image.
+    """
 
-condition.
-li title
-"""
+    print("clean individual records -- remove images with no-image placeholder")
+
+    # load input file
+    print("load input file")
+    in_p = os.path.join("..", "data", "individual_records.jsonl")
+    js = [json.loads(row) for row in open(in_p, "r").readlines()]
+
+    # create output file.
+    print("create output file")
+    of_p = os.path.join("..", "data", "cleaned_individual_records.jsonl")
+    of = open(of_p, "w", newline="")
+
+    print("clean and write")
+    n_rows_removed = 0
+    for j in js:
+
+        if not "no-image.jpg" in j["image_url"]:
+            #assert ".jpg" in j["image_url"] # make sure JPG
+            of.write(json.dumps(j) + "\n")
+        else:
+            n_rows_removed += 1
+    
+    # done.
+    of.flush()
+    of.close()
+    print("done, wrote cleaned_individual_records.jsonl")
+    print("removed {} rows with no image".format(n_rows_removed))
+
+def download_images():
+    """
+    For each record in cleaned_individual_records.jsonl, download the associated image.
+    It should be saved in data/{condition}/id_condition.jpg
+    """
+
+    # make 1 directory per conditin to hold images.
+    conditions = ["NM", "MINT", "EX", "VG", "GOOD", "FAIR", "POOR"]
+    for condition in conditions:
+       dir_p = os.path.join("..", "data", "imgs", condition)
+       if not os.path.exists(dir_p):
+            os.mkdir(dir_p)
+
+    # load individual records to get images for.
+    in_p = os.path.join("..", "data", "cleaned_individual_records.jsonl")
+    print("read {}".format(in_p))
+    js = [json.loads(row) for row in open(in_p, "r").readlines()]
+
+    # get images.
+    print("get images")
+    for idx, j in enumerate(js):
+
+        print(js)
+
+        # get url, id, condition.
+        img_url = j["image_url"]
+        id = j["id"]
+        condition = j["condition"]
+
+        # designate output file path.
+        fname = "{}_{}.jpg".format(id, condition)
+        of_p = os.path.join("..", "data", "imgs", fname)
+
+        # download image.
+        try:
+            resp = requests.get(img_url)
+            with open(of_p, "w") as of:
+                shutil.copyfileobj(resp.raw, of)
+
+        except Exception as e:
+            print(e, img_url)
+
+        # sleep 1s to be nice.
+        time.sleep(1)
+
+        # counter.
+        if idx % 100 == 0:
+            print(idx)
 
 if __name__ == "__main__":
     
     #download_beckett_htmls()
-    extract_individual_records()    
+    #extract_individual_records()    
+    #clean_individual_records()
+    download_images()
 
