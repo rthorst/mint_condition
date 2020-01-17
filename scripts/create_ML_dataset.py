8b53c17448d978a7b0d6209b71744a62c05b5c7d
Robert Thorstad <thorstadrs@gmail.com>
2020-01-17 15:03:34 -0500

    finish preprocessing of images

--- scripts/create_ML_dataset.py
+++ scripts/create_ML_dataset.py
@@ -29,11 +29,8 @@ def preprocess_img(PIL_object, output_shape=(255, 255, 3)):
     (new_width, new_height, new_channels) = output_shape
     #PIL_object = ImageOps.fit(PIL_object, (new_width, new_height), method=Image.ANTIALIAS)
     PIL_object = PIL_object.resize((new_width, new_height), Image.ANTIALIAS)
-    # Scale to int16 numpy array.
-    out = np.array(PIL_object, dtype=np.int16)
-
     # Return new image as numpy array.
-    return out
+    return np.array(PIL_object)
 
 def create_dataset_wrapper():
     """
@@ -41,54 +38,34 @@ def create_dataset_wrapper():
     """
 
     # List file paths to load.
-    # Also extract condition of the card from the filename.
-
+    # Also save original filename.
     print("list file paths to images in the dataset")
-    print("also extract condition of each card from the filename")
+    print("also extract filename of each card")
     print("currently keep only baseball images")
     img_paths = []
-    conditions = []
+    filenames = []
     base_p = os.path.join("..", "data", "imgs")
+
     for dir_name in os.listdir(base_p):
         print("\t...{}".format(dir_name))
         dir_p = os.path.join(base_p, dir_name)
 
         for img_fname in os.listdir(dir_p):
 
-            # skip non-baseball images.
-            if "sport185223" not in img_fname:
-                continue
-
             # get path.
             img_p = os.path.join(dir_p, img_fname)
             img_paths.append(img_p)
             
-            # get condition.
-            condition = img_fname.rstrip(".jpg").split("_")[-1]
-            condition = condition.lstrip("condition")
-            conditions.append(condition)
-
-            # break for testing.
-            if len(conditions) > 500:
-                print("break early for testing")
-                break
+            # get filename
+            filenames.append(img_fname)
 
     # Populate an empty array to hold images
     #of the final shape (255, 255, 3, n)
     # Also populate an empty array of target labels.
-    n = len(conditions)
-    X_shape = (n, 255, 255, 3)
+    n = len(filenames)
     final_img_shape = (255, 255, 3) # scale images to this shape.
-    y_shape = (n,)
 
-    print("create empty target array")
-    y = []
-    
-    print("create empty X array")
-    X = np.zeros(X_shape, dtype=np.int16)
-
-    # Start filling in this array with real images, 
-    # preprocessing as necessary.
+    # Preprocess images and write in matrix form.
     for idx, img_p in enumerate(img_paths):
 
         try:
@@ -102,42 +79,22 @@ def create_dataset_wrapper():
             # Cast to numpy arary.
             img_mtx = preprocess_img(img, final_img_shape)
 
-            # Visualize image for testing.
-            print(img_mtx.shape)
-            plt.imshow(img_mtx)
-            plt.show()
-
-            # Add to X, y array.
-            X[idx, :, :, :] = img_mtx
-            y.append(conditions[idx])
-
-            # Visualize image for testing.
-            to_show = X[idx]
-            print(to_show.shape)
-            plt.imshow(X[idx, :, :, :])
-            plt.show()
-
+            # Write output.
+            base_p = os.path.join("..", "data", "preprocessed_imgs")
+            out_fname = filenames[idx].replace(".jpg", "_preprocessed.npy")
+            out_p = os.path.join(base_p, out_fname)
+            np.save(file=out_p, arr=img_mtx)
 
         except Exception as e:
             print(e)
 
         # counter.
+        if idx == 0:
+            plt.imshow(img_mtx)
+            plt.title("Example image: does this look right?")
+            plt.show()
         if idx % 10 == 0:
-            print("\t...{}/{}".format(idx, n))
-        if idx > 100:
-            print("break for testing")
-            break
-
-    # Write dataset.
-    of_base_p = os.path.join("..", "data", "ml_dataset")
-    X_p = os.path.join(of_base_p, "X.npy")
-    y_p = os.path.join(of_base_p, "y.npy")
-
-    print("save y array")
-    np.save(file=y_p, arr=np.array(y))
-
-    print("save x array")
-    np.save(file=X_p, arr=X)
+            print("\t...preprocessed {}/{} images".format(idx, n))
 
     print("done")
 
