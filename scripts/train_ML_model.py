f9dbfd6b85ed9fb9dbd7ccb26a924f67938af9bb
Robert Thorstad <thorstadrs@gmail.com>
2020-01-19 16:34:16 -0500

    stable commit of train_ML_model.py which should successfully train

--- scripts/train_ML_model.py
+++ scripts/train_ML_model.py
@@ -24,11 +24,14 @@ https://stanford.edu/~shervine/blog/pytorch-how-to-generate-data-parallel
 
 """
 from sklearn.model_selection import train_test_split
+import copy
+import time
 import numpy as np
 import os
 import pickle 
 import torch
 from torchvision import transforms
+import torchvision.models as models
 from torch.utils import data 
 
 def split_train_test(test_size = 0.1):
@@ -124,27 +127,229 @@ class Dataset(data.Dataset):
 
     def __getitem__(self, index):
 
-        # Select sample.
-        ID = self.list_IDs[index]
+        try:
 
-        # Load X and reshape to (n_channels, height, width)
-        # Load data and get label.
-        base_p = os.path.join("..", "data", "preprocessed_imgs")
-        file_p = os.path.join(base_p, ID)
-        X = np.load(file_p)
-        X = torch.from_numpy(X).float()
-        X = np.reshape(X, (3, 255, 255))
-        y = self.labels[ID]
+            # Select sample.
+            ID = self.list_IDs[index]
 
-        # Normalize.
-        normalize = transforms.Normalize(mean=[0.485, 0.456, 0.406],
-                                 std=[0.229, 0.224, 0.225])
-        X = normalize(X)
+            # Get label.
+            y = self.labels[ID]
+
+            # Load X and reshape to (n_channels, height, width)
+            base_p = os.path.join("..", "data", "preprocessed_imgs")
+            file_p = os.path.join(base_p, ID)
+            X = np.load(file_p)
+            X = torch.from_numpy(X).float()
+            X = np.reshape(X, (3, 255, 255))
+
+            # Normalize.
+            normalize = transforms.Normalize(mean=[0.485, 0.456, 0.406],
+                                     std=[0.229, 0.224, 0.225])
+            X = normalize(X)
+
+        except Exception as e:
+            print("exception loading data..using random image, label instead")
+            X = np.random.random((3, 255, 255))
+            X = torch.from_numpy(X).float()
+            y = 0
 
         return X, y
 
 
-def train_CNN_model():
+def set_parameter_requires_grad(model, feature_extracting):
+    """
+    Helper function. If we are feature extracting, e.g. fine-tuning
+    only the top layer of a model, then set existing parameters in the
+    model to not return gradients.
+    From 
+    https://pytorch.org/tutorials/beginner/finetuning_torchvision_models_tut    orial.html
+    """
+    if feature_extracting:
+        for param in model.parameters():
+            param.requires_grad = False
+
+def initialize_model(model_name, num_classes, feature_extract, use_pretrained=True):
+
+    """
+    Helper function to initialize pretrained AlexNet (and other vision)
+    models in Pytorch.
+    From https://pytorch.org/tutorials/beginner/finetuning_torchvision_models_tutorial.html
+    """
+
+    # Initialize these variables which will be set in this if statement. Each of these
+    #   variables is model specific.
+    model_ft = None
+    input_size = 0
+
+    if model_name == "resnet":
+        """ Resnet18
+        """
+        model_ft = models.resnet18(pretrained=use_pretrained)
+        set_parameter_requires_grad(model_ft, feature_extract)
+        num_ftrs = model_ft.fc.in_features
+        model_ft.fc = nn.Linear(num_ftrs, num_classes)
+        input_size = 224
+
+    elif model_name == "alexnet":
+        """ Alexnet
+        """
+        model_ft = models.alexnet(pretrained=use_pretrained)
+        set_parameter_requires_grad(model_ft, feature_extract)
+        num_ftrs = model_ft.classifier[6].in_features
+        model_ft.classifier[6] = torch.nn.Linear(num_ftrs,num_classes)
+        input_size = 224
+
+    elif model_name == "vgg":
+        """ VGG11_bn
+        """
+        model_ft = models.vgg11_bn(pretrained=use_pretrained)
+        set_parameter_requires_grad(model_ft, feature_extract)
+        num_ftrs = model_ft.classifier[6].in_features
+        model_ft.classifier[6] = nn.Linear(num_ftrs,num_classes)
+        input_size = 224
+
+    elif model_name == "squeezenet":
+        """ Squeezenet
+        """
+        model_ft = models.squeezenet1_0(pretrained=use_pretrained)
+        set_parameter_requires_grad(model_ft, feature_extract)
+        model_ft.classifier[1] = nn.Conv2d(512, num_classes, kernel_size=(1,1), stride=(1,1))
+        model_ft.num_classes = num_classes
+        input_size = 224
+
+    elif model_name == "densenet":
+        """ Densenet
+        """
+        model_ft = models.densenet121(pretrained=use_pretrained)
+        set_parameter_requires_grad(model_ft, feature_extract)
+        num_ftrs = model_ft.classifier.in_features
+        model_ft.classifier = nn.Linear(num_ftrs, num_classes)
+        input_size = 224
+
+    elif model_name == "inception":
+        """ Inception v3
+        Be careful, expects (299,299) sized images and has auxiliary output
+        """
+        model_ft = models.inception_v3(pretrained=use_pretrained)
+        set_parameter_requires_grad(model_ft, feature_extract)
+        # Handle the auxilary net
+        num_ftrs = model_ft.AuxLogits.fc.in_features
+        model_ft.AuxLogits.fc = nn.Linear(num_ftrs, num_classes)
+        # Handle the primary net
+        num_ftrs = model_ft.fc.in_features
+        model_ft.fc = nn.Linear(num_ftrs,num_classes)
+        input_size = 299
+
+    else:
+        print("Invalid model name, exiting...")
+        exit()
+
+    return model_ft, input_size
+
+
+def train_model(model, dataloaders, criterion, optimizer, num_epochs=25, is_inception=False, device="cpu"):
+    """ Helper function to train model in PyTorch
+    https://pytorch.org/tutorials/beginner/finetuning_torchvision_models_tut    orial.html
+    """
+
+    since = time.time()
+
+    val_acc_history = []
+
+    best_model_wts = copy.deepcopy(model.state_dict())
+    best_acc = 0.0
+
+    for epoch in range(num_epochs):
+        print('Epoch {}/{}'.format(epoch, num_epochs - 1))
+        print('-' * 10)
+
+        # Each epoch has a training and validation phase
+        for phase in ['train', 'test']:
+            if phase == 'train':
+                model.train()  # Set model to training mode
+            else:
+                model.eval()   # Set model to evaluate mode
+
+            running_loss = 0.0
+            running_corrects = 0.0
+
+            # Iterate over data.
+            batch_num = 0
+            for inputs, labels in dataloaders[phase]:
+                inputs = inputs.to(device)
+                labels = labels.to(device)
+    
+                # zero the parameter gradients
+                optimizer.zero_grad()
+
+                # forward
+                # track history if only in train
+                with torch.set_grad_enabled(phase == 'train'):
+                    # Get model outputs and calculate loss
+                    # Special case for inception because in training it has an auxiliary output. In train
+                    #   mode we calculate the loss by summing the final output and the auxiliary output
+                    #   but in testing we only consider the final output.
+                    if is_inception and phase == 'train':
+                        # From https://discuss.pytorch.org/t/how-to-optimize-inception-model-with-auxiliary-classifiers/7958
+                        outputs, aux_outputs = model(inputs)
+                        loss1 = criterion(outputs, labels)
+                        loss2 = criterion(aux_outputs, labels)
+                        loss = loss1 + 0.4*loss2
+                    else:
+                        outputs = model(inputs)
+                        loss = criterion(outputs, labels)
+
+                    _, preds = torch.max(outputs, 1)
+
+                    # backward + optimize only if in training phase
+                    if phase == 'train':
+                        loss.backward()
+                        optimizer.step()
+
+                # statistics
+                running_loss += loss.item() * inputs.size(0)
+                running_corrects += torch.sum(preds == labels.data)
+
+                # counter.
+                batch_num += 1
+
+                if batch_num % 5 == 0:
+                    correct = float(torch.sum(preds == labels.data))
+                    incorrect = float(torch.sum(preds != labels.data))
+                    perc_correct = 100 * correct / (correct + incorrect)
+                    msg = """
+                    batch {} : percent correct {}
+                    """.format(batch_num, perc_correct)
+                    print(msg)
+
+            epoch_loss = running_loss / len(dataloaders[phase].dataset)
+            epoch_acc = running_corrects.double() / len(dataloaders[phase].dataset)
+
+            print('{} Loss: {:.4f} Acc: {:.4f}'.format(phase, epoch_loss, epoch_acc))
+
+            # deep copy the model
+            if phase == 'val' and epoch_acc > best_acc:
+                best_acc = epoch_acc
+                best_model_wts = copy.deepcopy(model.state_dict())
+            if phase == 'val':
+                val_acc_history.append(epoch_acc)
+
+        # Pickle the model after end of epoch.
+        of_p = os.path.join("..", "models", "latest_model.p")
+        with open(of_p, "wb") as of:
+            pickle.dump(model, of)
+        print("wrote {}".format(of_p))
+
+    time_elapsed = time.time() - since
+    print('Training complete in {:.0f}m {:.0f}s'.format(time_elapsed // 60, time_elapsed % 60))
+    print('Best val Acc: {:4f}'.format(best_acc))
+
+    # load best model weights
+    model.load_state_dict(best_model_wts)
+    return model, val_acc_history
+
+
+def train_CNN_model(num_classes=7):
     """
     Train a CNN to classify card pictures.
     """ 
@@ -187,42 +392,46 @@ def train_CNN_model():
     test_set = Dataset(partition["test"], labels)
     test_g = data.DataLoader(test_set, **params)
 
+    # Initialize pretrained AlexNet model 
+    # Replace top layer with a layer of (num_classes) outputs
+    # Note that setting feature_extract to False means we fine-tune
+    # the whole model, True we finetune only top layer.
+    model, _ = initialize_model(
+        model_name = "alexnet",
+        num_classes = num_classes,
+        feature_extract = True, # if True only finetune top layer.
+        use_pretrained = True
+    )
+    model.to(device)
+
+    # Initialize optimizer, loss criterion.
+    optimizer = torch.optim.Adam(model.parameters())
+    criterion = torch.nn.CrossEntropyLoss()
 
     # Train model.
-    print("train model")
-    batch_num = 0
-    for epoch in range(max_epochs):
+    model, history = train_model(
+        model=model,
+        dataloaders = {"train" : train_g, "test" : test_g},
+        criterion=criterion,
+        optimizer=optimizer,
+        num_epochs=max_epochs,
+        is_inception=False,
+        device=device
+    )
 
-        print("---epoch {}---".format(epoch))
+    # Pickle best performing model.
+    of_p = os.path.join("..", "models", "best_model.p")
+    with open(of_p, "wb") as of:
+        pickle.dump(model, of)
+    print("wrote {}".format(of_p))
 
-        # Train.
-        for local_batch, local_labels in train_g:
+    # Pickle history of best performing model.
+    of_p = os.path.join("..", "models", "history.p")
+    with open(of_p, "wb") as of:
+        pickle.dump(history, of)
+    print("wrote {}".format(of_p))
 
-            # Transfer to GPU if available.
-            local_batch = local_batch.to(device)
-            local_labels = local_labels.to(device)
 
-            """
-            Model computations. TODO fill me out.
-            """
-
-            # Counter.
-            batch_num += 1
-            if batch_num % 5 == 0:
-                print("...batch number {}".format(batch_num))
-
-        # Test.
-        with torch.set_grad_enabled(False):
-
-            for local_batch, local_labels in test_g:
-
-                # Transfer to GPU if available.
-                local_batch = local_batch.to(device)
-                local_labels = local_labels.to(device)
-        
-                """
-                Model computations. TODO fill me out.
-                """
 if __name__ == "__main__":
 
     split_train_test()
