1e6580434244d414cd9a21ae239c05f1c5317062
Robert Thorstad <thorstadrs@gmail.com>
2020-01-27 13:57:51 -0500

    unstable commit...modifications to front-end to use ebay-derived model

--- front_end/streamlit_frontend.py
+++ front_end/streamlit_frontend.py
@@ -9,15 +9,131 @@ from scipy.ndimage import gaussian_filter
 from flashtorch.saliency import Backprop
 import seaborn as sns
 import matplotlib.pyplot as plt
+from torchvision import models
 
 #####################
 ## Helper functions #
 ####################
+
+def set_parameter_requires_grad(model, feature_extracting):
+    """
+    Helper function to freeze layers when fine-tuning a model.
+    Sets all neurons except those in a top layer to not return gradient.
+
+    From 
+    https://pytorch.org/tutorials/beginner/finetuning_torchvision_models_tut    orial.html
+    """
+    if feature_extracting:
+        for param in model.parameters():
+            param.requires_grad = False
+
+
+def initialize_model(model_name, num_classes, feature_extract, use_pretrained=True):
+
+    """
+    Helper function to initialize pretrained AlexNet (and other vision)
+    models in Pytorch.
+    From https://pytorch.org/tutorials/beginner/finetuning_torchvision_models_tutorial.html
+    """
+
+    # Initialize these variables which will be set in this if statement. Each of these
+    #   variables is model specific.
+    model_ft = None
+    input_size = 0
+
+    if model_name == "resnet":
+        """ Resnet18
+        """
+        model_ft = models.resnet18(pretrained=use_pretrained)
+        set_parameter_requires_grad(model_ft, feature_extract)
+        num_ftrs = model_ft.fc.in_features
+        model_ft.fc = torch.nn.Linear(num_ftrs, num_classes)
+        input_size = 224
+
+    elif model_name == "alexnet":
+        """ Alexnet
+        """
+        model_ft = models.alexnet(pretrained=use_pretrained)
+        set_parameter_requires_grad(model_ft, feature_extract)
+        num_ftrs = model_ft.classifier[6].in_features
+        model_ft.classifier[6] = torch.nn.Linear(num_ftrs,num_classes)
+        input_size = 224
+
+    elif model_name == "vgg":
+        """ VGG11_bn
+        """
+        model_ft = models.vgg11_bn(pretrained=use_pretrained)
+        set_parameter_requires_grad(model_ft, feature_extract)
+        num_ftrs = model_ft.classifier[6].in_features
+        model_ft.classifier[6] = nn.Linear(num_ftrs,num_classes)
+        input_size = 224
+
+    elif model_name == "squeezenet":
+        """ Squeezenet
+        """
+        model_ft = models.squeezenet1_0(pretrained=use_pretrained)
+        set_parameter_requires_grad(model_ft, feature_extract)
+        model_ft.classifier[1] = nn.Conv2d(512, num_classes, kernel_size=(1,1), stride=(1,1))
+        model_ft.num_classes = num_classes
+        input_size = 224
+
+    elif model_name == "densenet":
+        """ Densenet
+        """
+        model_ft = models.densenet121(pretrained=use_pretrained)
+        set_parameter_requires_grad(model_ft, feature_extract)
+        num_ftrs = model_ft.classifier.in_features
+        model_ft.classifier = nn.Linear(num_ftrs, num_classes)
+        input_size = 224
+
+    elif model_name == "inception":
+        """ Inception v3
+        Be careful, expects (299,299) sized images and has auxiliary output
+        """
+        model_ft = models.inception_v3(pretrained=use_pretrained)
+        set_parameter_requires_grad(model_ft, feature_extract)
+        # Handle the auxilary net
+        num_ftrs = model_ft.AuxLogits.fc.in_features
+        model_ft.AuxLogits.fc = nn.Linear(num_ftrs, num_classes)
+        # Handle the primary net
+        num_ftrs = model_ft.fc.in_features
+        model_ft.fc = nn.Linear(num_ftrs,num_classes)
+        input_size = 299
+
+    else:
+        print("Invalid model name, exiting...")
+        exit()
+
+    return model_ft, input_size
+
+
+#def preload_ml_model():
+#    # helper function to preload ML model
+#    model_p = os.path.join("..", "models", "cloud_model.p")
+#    model = pickle.load(open(model_p, "rb"))
+#    return model
+
 def preload_ml_model():
-    """ helper function to preload ML model """
-    model_p = os.path.join("..", "models", "cloud_model.p")
-    model = pickle.load(open(model_p, "rb"))
+    """
+    helper function to preload ML model.
+
+    returns:
+    ---------
+    model (torch model object)
+
+    Note that the model is serialized, as a "state dict", using
+    Python 2.7. Thus special flags need to be used to un-serialize
+    the model in Python 3.
+    """
+
+    # Load model.
+    model_p = os.path.join("..", "models", "ebay_model.p")
+    model = torch.load(model_p, 
+                       map_location = torch.device("cpu")
+            )
+
     return model
+
 
 def load_and_preprocess_img(img_p):
     """
@@ -102,6 +218,7 @@ def predict(model, img_p):
         confidence = 100 * np.amax(pred_probs)
 
     # Get string label.
+    """
     int_to_lbl = {
         0 : "Mint",
         1 : "Near Mint",
@@ -109,6 +226,17 @@ def predict(model, img_p):
         3 : "Very Good",
         4 : "Poor"
     }
+    """
+
+    int_to_lbl = {
+        4 : "Mint",
+        3 : "Near Mint",
+        2 : "Excellent",
+        1 : "Very Good",
+        0 : "Poor"
+    }
+
+
     ypred_str = int_to_lbl[ypred_int]
 
     return ypred_str, confidence
