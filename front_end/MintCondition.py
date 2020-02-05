daf6eff9a6e4c6b0bc09d1166d534b5c904d4ed9
Ubuntu <ubuntu@ip-172-31-36-89.ec2.internal>
2020-02-05 02:51:19 +0000

    remove extraneous file from front-end for manually re-running streamlit model

--- front_end/MintCondition.py
+++ front_end/MintCondition.py
@@ -36,8 +36,9 @@ def preload_ml_model():
 
     # Load model.
     #model_p = os.path.join("..", "models", "resnet_62_acc_torchsave.p")
-    #model_p = os.path.join("..", "models", "10class_45_acc_torchsave.p")
-    model_p = os.path.join("..", "models", "10class_64_acc.p")
+    #model_p = os.path.join("..", "models", "10class_58_acc.p")
+    #model_p = os.path.join("..", "models", "10class_74_acc.p")
+    model_p = os.path.join("..", "models", "10class_45_acc_torchsave.p")
     model = torch.load(model_p, 
                        map_location = torch.device("cpu")
             )
@@ -209,7 +210,7 @@ file = streamlit.file_uploader(label="Option 1: Upload a Picture of the Card")
 
 ## Get an image from ebay.
 ebay_md = """
-Option 2: enter an ebay auction URL e.g. http://www.shorturl.at/MQWY8
+Option 2: enter an ebay auction URL.
 """
 #streamlit.markdown(ebay_md)
 ebay_url = streamlit.text_input(ebay_md)
